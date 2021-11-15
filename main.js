6aac265d61caf1e8e116bc440dc6e5065b223649
GianlucaPolcino <gianluca.polcino.gp@gmail.com>
2021-11-15 15:41:47 +0100

    layout in beta

---
+++ main.js
@@ -0,0 +1,37 @@
+const app = new Vue({
+    el: '#app',
+
+    data:{
+
+        contacts:[
+            {
+                name: 'Michele',
+                image: '_1',
+                hidden: true,
+                message: [],
+            },
+
+            {
+                name: 'Fabio',
+                image: '_2',
+                hidden: true,
+                message: [],
+            },
+
+            {
+                name: 'Samuele',
+                image: '_3',
+                hidden: true,
+                message: [],
+            },
+
+            {
+                name: 'Luisa',
+                image: '_4',
+                hidden: true,
+                message: [],
+            },
+        ]
+    },
+
+})
